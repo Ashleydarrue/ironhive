de32ec4899309900f2350566b6c821ba73ca2210
Ashley Arrue <ashleydarrue@gmail.com>
2019-06-25 17:02:03 -0400

    squshed counter added

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 let allTheBeetles = [];
 window.onload = function () {
   ctx = document.getElementById('canvas').getContext('2d');
+  
+
 
   class Beetle {
     constructor(beetleX, beetleY, beetleWidth, beetleHeight) {
@@ -15,30 +17,30 @@ window.onload = function () {
     }
     drawItself() {
       this.counter++;
-      
+
       this.image = this.images[this.counter];
-      
+
       if (this.counter == 7) {
         this.counter = -1
       }
-      
+
       let theImage = new Image();
       theImage.src = this.image;
-      
+
       ctx.drawImage(theImage, this.x, this.y, 45, 50);
     }
 
     moveAcrossForever() {
       setInterval(() => {
         this.x -= 4;
-        
+
         if (this.direction == 2) {
           this.y -= 4
         }
         if (this.direction == 0) {
           this.y += 4
         }
-        
+
         let randomNum = Math.floor(Math.random() * 12)
         if (randomNum == 0 || randomNum == 2) {
           if (this.direction == 2) {
@@ -50,16 +52,19 @@ window.onload = function () {
           }
         }
       }, 200)
-    } 
+    }
   }
-  
+
   function drawEverything() {
+    
     allTheBeetles.forEach((eachBeetle) => {
       eachBeetle.drawItself();
+      
     })
+    flower.drawFlower();
   }
-  
-  
+
+
   function animate() {
     ctx.clearRect(0, 0, 1430, 675);
     let randomNum = Math.floor(Math.random() * 50)
@@ -70,41 +75,67 @@ window.onload = function () {
       beetles.moveAcrossForever();
     }
     drawEverything();
-    requestAnimationFrame(animate);  
+    requestAnimationFrame(animate);
   }
-  animate();
- 
- 
+
+  setTimeout(()=>{
+    animate();
+  }, 5000)
   
- /// make this a game class
+
+ let hitNum = 0;
+
+  /// make this a game class
   canvas.addEventListener("click", function (evt) {
     var mousePos = getMousePos(canvas, evt);
     console.log(Math.floor(mousePos.x) + ',,,,,,,' + Math.floor(mousePos.y));
-    for(let i = 0; i < allTheBeetles.length; i++) {
+    for (let i = 0; i < allTheBeetles.length; i++) {
       // console.log("------- ", allTheBeetles[i].x, allTheBeetles[i].y, allTheBeetles[i].width, allTheBeetles[i].height);
-      if(Math.floor(mousePos.x) < allTheBeetles[i].x + (allTheBeetles[i].width) && Math.floor(mousePos.x) > allTheBeetles[i].x && Math.floor(mousePos.y) < allTheBeetles[i].y + (allTheBeetles[i].height - 7) && Math.floor(mousePos.y) > allTheBeetles[i].y) {
-        console.log("Hit Hit Hit Hit Hit Hit");
-        allTheBeetles.splice(i, 1)
+      if (Math.floor(mousePos.x) < allTheBeetles[i].x + (allTheBeetles[i].width) && Math.floor(mousePos.x) > allTheBeetles[i].x && Math.floor(mousePos.y) < allTheBeetles[i].y + (allTheBeetles[i].height - 7) && Math.floor(mousePos.y) > allTheBeetles[i].y) {
+        allTheBeetles.splice(i, 1) 
+        hitNum++
+        document.getElementById('hit').innerHTML= hitNum
       }
     }
   }, false);
 
-//Get Mouse Position
- function getMousePos(canvas, evt) {
+  //Get Mouse Position
+  function getMousePos(canvas, evt) {
     var rect = canvas.getBoundingClientRect();
     return {
-        x: evt.clientX - rect.left,
-        y: evt.clientY - rect.top
+      x: evt.clientX - rect.left,
+      y: evt.clientY - rect.top
     };
- }
- 
- 
-   class Sunflower  {
-     constructor (){
+  }
 
-     }
-   } 
+// sunflower 
 
+  class Sunflower {
+    constructor(sunflowerX, sunflowerY, sunflowerWidth, sunflowerHeight) {
+      this.x = sunflowerX;
+      this.y = sunflowerY;
+      this.width = sunflowerWidth;
+      this.height = sunflowerHeight;
+      this.images = ['./../image/sunflower1.png' , './../image/sunflower2.png', "./../image/sunflower3.png", "./../image/sunflower4.png", "./../image/sunflower5.png"];
+      this.image = this.images[0];
+      this.counter = -1;
+    }
 
+    drawFlower() {
+      this.counter++;
 
+      this.image = this.images[this.counter];
+
+      if (this.counter == 4) {
+        this.counter = -1
+      }
+
+      let theImage = new Image();
+      theImage.src = this.image;
+
+      ctx.drawImage(theImage, 200, 1400, 100, 150);
+    }
+
+  }
+  let flower = new Sunflower(200,1400, 50,50);
 }
